8b724bb380af725484c9e82162e82e99723fb6f9
DJY1873 <djy1873@gmail.com>
2024-01-07 15:50:04 +0800

    parameter_control_code

---
+++ main/parameter_control.h
@@ -0,0 +1,11 @@
+#ifdef __PARAMETER_CONTORL__
+#define __PARAMETER_CONTORL__
+
+extern float Kp_b;
+extern float Kd_b;
+extern float Kp_s;
+extern float Ki_s;
+extern float Kp_d;
+extern float Kd_d;
+
+#endif // __PARAMETER_CONTORL__
